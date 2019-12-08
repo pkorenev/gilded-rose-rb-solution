144110d6b8c24a6131f0189993d5dc14f3466ad2
pkorenev <p.korenev@voroninstudio.eu>
2019-12-08 21:10:53 +0200

    use constants for item names

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,20 +1,28 @@
+require 'ostruct'
+
 class GildedRose
+  ITEM_NAMES = OpenStruct.new(
+    aged_brie: 'Aged Brie',
+    backstage: 'Backstage passes to a TAFKAL80ETC concert',
+    sulfuras: 'Sulfuras, Hand of Ragnaros'
+  ).freeze
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      if item.name != ITEM_NAMES.aged_brie and item.name != ITEM_NAMES.backstage
         if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
+          if item.name != ITEM_NAMES.sulfuras
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.name == ITEM_NAMES.backstage
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -28,14 +36,14 @@ class GildedRose
           end
         end
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
+      if item.name != ITEM_NAMES.sulfuras
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.name != ITEM_NAMES.aged_brie
+          if item.name != ITEM_NAMES.backstage
             if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
+              if item.name != ITEM_NAMES.sulfuras
                 item.quality = item.quality - 1
               end
             end
