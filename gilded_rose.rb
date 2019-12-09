2c2ef717f6862e2dda175f7614b41767fbc0831a
pkorenev <p.korenev@voroninstudio.eu>
2019-12-09 12:19:28 +0200

    add tests and rewrite #update_quality method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,62 +1,93 @@
 require 'ostruct'
 
 class GildedRose
-  ITEM_NAMES = OpenStruct.new(
-    aged_brie: 'Aged Brie',
-    backstage: 'Backstage passes to a TAFKAL80ETC concert',
-    sulfuras: 'Sulfuras, Hand of Ragnaros'
-  ).freeze
-
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != ITEM_NAMES.aged_brie and item.name != ITEM_NAMES.backstage
-        if item.quality > 0
-          if item.name != ITEM_NAMES.sulfuras
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == ITEM_NAMES.backstage
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != ITEM_NAMES.sulfuras
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != ITEM_NAMES.aged_brie
-          if item.name != ITEM_NAMES.backstage
-            if item.quality > 0
-              if item.name != ITEM_NAMES.sulfuras
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      update_item(item)
+    end
+  end
+
+  private
+
+  ITEM_NAMES = OpenStruct.new(
+    aged_brie: 'Aged Brie',
+    backstage: 'Backstage passes to a TAFKAL80ETC concert',
+    sulfuras: 'Sulfuras, Hand of Ragnaros',
+    conjured: 'Conjured'
+  ).freeze
+
+  MIN_QUALITY = 0.freeze
+  MAX_QUALITY = 50.freeze
+  LEGENDARY_QUALITY = 80.freeze
+  BACKSTAGE_BREAKPOINTS = [
+    {sell_in: 5, quality_increment: 3},
+    {sell_in: 10, quality_increment: 2}
+  ].freeze
+
+  def update_item(item)
+    item.quality = calculate_new_quality(item)
+    item.sell_in = calculate_new_sell_in(item)
+  end
+
+  def calculate_new_quality(item)
+    return LEGENDARY_QUALITY if legendary_item?(item)
+    if item.sell_in <= 0
+      if item.name == ITEM_NAMES.backstage
+        return 0
+      elsif item.name == ITEM_NAMES.conjured
+        return normalize_quality(item.quality - 4)
+      elsif regular_item?(item)
+        return normalize_quality(item.quality - 2)
       end
     end
+
+    new_quality = nil
+
+    if regular_item?(item)
+      new_quality = item.quality - 1
+    elsif item.name == ITEM_NAMES.conjured
+      new_quality = item.quality - 2
+    elsif item.name == ITEM_NAMES.aged_brie
+      new_quality = item.quality + 1
+    elsif item.name == ITEM_NAMES.backstage
+      breakpoint = BACKSTAGE_BREAKPOINTS.find { |bp| item.sell_in <= bp[:sell_in] }
+      quality_increment = breakpoint ? breakpoint[:quality_increment] : 1
+      new_quality = item.quality + quality_increment
+    end
+
+    normalize_quality(new_quality)
+  end
+
+  def normalize_quality(new_quality)
+    if new_quality < MIN_QUALITY
+      MIN_QUALITY
+    elsif new_quality > MAX_QUALITY
+      MAX_QUALITY
+    else
+      new_quality
+    end
+  end
+
+  def calculate_new_sell_in(item)
+    return nil if unlimited_sell_in?(item)
+
+    item.sell_in - 1
+  end
+
+  def regular_item?(item)
+    !ITEM_NAMES.to_h.values.include?(item.name)
+  end
+
+  def unlimited_sell_in?(item)
+    legendary_item?(item) || item.name == ITEM_NAMES.aged_brie
+  end
+
+  def legendary_item?(item)
+    item.name == ITEM_NAMES.sulfuras
   end
 end
 
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,180 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  describe '#update_quality' do
+  context '#update_quality' do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'foo'
+    end
+
+    context 'for regular item' do
+      it 'with non-zero quality and non-zero sell_in' do
+        items = [Item.new('foo', 20, 30)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(19)
+        expect(items[0].quality).to eq(29)
+      end
+
+      it 'with zero quality' do
+        items = [Item.new('foo', 20, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(19)
+        expect(items[0].quality).to eq(0)
+      end
+
+      context 'with sell_in <= 0' do
+        it do
+          items = [Item.new('foo', 0, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+          expect(items[0].quality).to eq(28)
+        end
+
+        it do
+          items = [Item.new('foo', -5, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-6)
+          expect(items[0].quality).to eq(8)
+        end
+      end
+    end
+
+    context 'for "Aged Brie"' do
+      it 'with non-max quality' do
+        items = [Item.new('Aged Brie', 0, 30)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(nil)
+        expect(items[0].quality).to eq(31)
+      end
+
+      it 'with max quality' do
+        items = [Item.new('Aged Brie', 0, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(nil)
+        expect(items[0].quality).to eq(50)
+      end
+    end
+
+    context 'for "Backstage passes"' do
+      it 'with sell_in > 10' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 30)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(10)
+        expect(items[0].quality).to eq(31)
+      end
+
+      context 'with sell_in between 6 and 10' do
+        it do
+          items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(9)
+          expect(items[0].quality).to eq(32)
+        end
+
+        it do
+          items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 6, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(5)
+          expect(items[0].quality).to eq(32)
+        end
+      end
+
+      context 'with sell_in between 1 and 5' do
+        it do
+          items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 1, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(0)
+          expect(items[0].quality).to eq(33)
+        end
+
+        it do
+          items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(4)
+          expect(items[0].quality).to eq(33)
+        end
+      end
+
+      context 'with zero sell_in' do
+        it do
+          items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+          expect(items[0].quality).to eq(0)
+        end
+
+        it do
+          items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+          expect(items[0].quality).to eq(0)
+        end
+      end
+
+      it 'with max quality' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(9)
+        expect(items[0].quality).to eq(50)
+      end
+    end
+
+    context 'for "Sulfuras"' do
+      context 'with any initial quality and sell_in' do
+        it do
+          items = [Item.new('Sulfuras, Hand of Ragnaros', 20, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(nil)
+          expect(items[0].quality).to eq(80)
+        end
+
+        it do
+          items = [Item.new('Sulfuras, Hand of Ragnaros', 20, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(nil)
+          expect(items[0].quality).to eq(80)
+        end
+
+        it do
+          items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(nil)
+          expect(items[0].quality).to eq(80)
+        end
+      end
+    end
+
+    context 'for "Conjured"' do
+      it 'with non-zero quality and non-zero sell_in' do
+        items = [Item.new('Conjured', 20, 30)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(19)
+        expect(items[0].quality).to eq(28)
+      end
+
+      it 'with zero quality' do
+        items = [Item.new('Conjured', 20, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(19)
+        expect(items[0].quality).to eq(0)
+      end
+
+      context 'with zero sell_in' do
+        it do
+          items = [Item.new('Conjured', 0, 30)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+          expect(items[0].quality).to eq(26)
+        end
+
+        it do
+          items = [Item.new('Conjured', 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+          expect(items[0].quality).to eq(46)
+        end
+      end
     end
   end
 end
